2b41e04b9f491ca2c797e28e2709c5519cda9631
Drikus Roor <drikusroor@gmail.com>
2023-10-31 12:24:16 +0100

    Added: Convert to svelte kit (#1)
    * config: Initialize sveltekit project
    
    * chore: Replace favicon
    
    * chore: Move audio assets to static folder
    
    * chore: Remove old bun server
    
    * config: Configure tailwindcss
    
    * feat: Convert original page to svelte page
    
    * fix: Fix search function
    
    * feat: Make base flow work again
    
    * refactor: Convert page to use typescript
    
    * refactor: Correctly type currentTimeout variable
    
    * type: Correctly type sentences
    
    * refactor: Move to data oriented flash cards with accompanying types
    
    * refactor: Make switching languages and modes reactive with Svelte
    
    * refactor: Make languages more data-based

---
+++ src/routes/japanese/+page.svelte
@@ -0,0 +1,439 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	import dataJson from './data.json';
+
+	type TranslationKeys =
+		| 'title'
+		| 'mostImportantWords'
+		| 'answer'
+		| 'submitAnswerButton'
+		| 'correct'
+		| 'incorrect'
+		| 'correctAnswers'
+		| 'nextQuestion'
+		| 'search'
+		| 'fromLinkText'
+		| 'toLinkText';
+
+	interface Language {
+		code: string;
+		label: string;
+		default?: boolean;
+	}
+
+	interface FlashCardsConfigurationData {
+		languages: Language[];
+		translations: {
+			[language: string]: { [key in TranslationKeys]: string };
+		};
+		questions: Sentence[];
+	}
+
+	const data: FlashCardsConfigurationData = dataJson;
+
+	type LanguageCode = (typeof languages)[number]['code'];
+
+	interface Sentence {
+		q: string;
+		[key: LanguageCode]: string[];
+	}
+
+	type Mistakes = { [question: string]: number };
+
+	let languages = data.languages;
+	const defaultLanguage = languages.find((language) => language.default);
+
+	let sentences: FlashCardsConfigurationData['questions'];
+	let translations: FlashCardsConfigurationData['translations'] = data.translations;
+	let t: (key: TranslationKeys, defaultValue?: string) => string;
+	let currentLanguage: LanguageCode;
+	let learningMode: 'from' | 'to';
+
+	// QA flow related
+	let questionBoxClasses = '';
+	let questionText = '';
+	let answerInputValue = '';
+	let currentSentence: Sentence;
+	let mistakes: Mistakes = {};
+	let isFeedbackDisplayed = false;
+	let currentTimeout: NodeJS.Timeout;
+	let submitAnswerButtonText = '';
+	let feedbackText = '';
+
+	// Cheatsheet / search related
+	let filteredSentences: FlashCardsConfigurationData['questions'] = [];
+	let searchInputValue = '';
+
+	// Streak related
+	let streakText = 0;
+	let streakClasses = 'hidden';
+
+	function handleAnswerSubmit(e: Event) {
+		e.preventDefault();
+
+		if (isFeedbackDisplayed) {
+			loadNextSentence();
+		} else {
+			checkAnswer();
+		}
+	}
+
+	function loadNextSentence() {
+		currentTimeout ? clearTimeout(currentTimeout) : null;
+		currentSentence = getNextSentence();
+		questionText =
+			learningMode === 'from' ? currentSentence.q : currentSentence[currentLanguage].join(' / ');
+		answerInputValue = '';
+		feedbackText = '';
+		isFeedbackDisplayed = false;
+		submitAnswerButtonText = getTranslation('submitAnswerButton', 'Submit');
+		questionBoxClasses = 'border-purple-600';
+	}
+
+	function getNextSentence() {
+		let prioritizedSentences = shuffle(sentences)
+			.filter((sentence) => sentence.q !== currentSentence?.q)
+			.sort((a, b) => {
+				if (mistakes[a.q] && mistakes[b.q]) {
+					return mistakes[a.q] - mistakes[b.q];
+				}
+
+				if (mistakes[a.q]) {
+					return -1;
+				}
+
+				if (mistakes[b.q]) {
+					return 1;
+				}
+
+				return 0;
+			})
+			.slice(0, 10);
+
+		if (prioritizedSentences.length) {
+			return prioritizedSentences[Math.floor(Math.random() * prioritizedSentences.length)];
+		}
+
+		return sentences[Math.floor(Math.random() * sentences.length)];
+	}
+
+	function getTranslation(key: TranslationKeys, defaultValue?: string) {
+		const translation = translations[currentLanguage][key] || translations['general'][key];
+
+		if (translation) {
+			return translation;
+		}
+
+		if (defaultValue) {
+			return defaultValue;
+		}
+
+		return `{{${key}}}`;
+	}
+
+	$: t = (key: TranslationKeys, defaultValue?: string) => {
+		if (translations && translations[currentLanguage]) {
+			return getTranslation(key, defaultValue);
+		}
+
+		return defaultValue || `{{${key}}}`;
+	};
+
+	function shuffle<T>(array: T[] = []): T[] {
+		let currentIndex = array.length,
+			randomIndex;
+
+		// While there remain elements to shuffle.
+		while (currentIndex > 0) {
+			// Pick a remaining element.
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex--;
+
+			// And swap it with the current element.
+			[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+		}
+
+		return array;
+	}
+
+	function levenshtein(a: string, b: string) {
+		const matrix = [];
+
+		if (a.length == 0) return b.length;
+		if (b.length == 0) return a.length;
+
+		for (let i = 0; i <= b.length; i++) {
+			matrix[i] = [i];
+		}
+
+		for (let j = 0; j <= a.length; j++) {
+			matrix[0][j] = j;
+		}
+
+		for (let i = 1; i <= b.length; i++) {
+			for (let j = 1; j <= a.length; j++) {
+				if (b.charAt(i - 1) == a.charAt(j - 1)) {
+					matrix[i][j] = matrix[i - 1][j - 1];
+				} else {
+					matrix[i][j] = Math.min(
+						matrix[i - 1][j - 1] + 1,
+						Math.min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1)
+					);
+				}
+			}
+		}
+
+		return matrix[b.length][a.length];
+	}
+
+	function updateStreak(increment = false) {
+		if (increment) {
+			streakText = streakText + 1;
+			streakClasses = 'bg-green-500';
+		} else {
+			streakText = 0;
+			streakClasses = 'bg-pink-500';
+		}
+	}
+
+	function checkAnswer() {
+		const userAnswer = answerInputValue.trim();
+		const correctAudio = document.getElementById('correctAudio') as HTMLAudioElement;
+		const incorrectAudio = document.getElementById('incorrectAudio') as HTMLAudioElement;
+		let isCorrect = false;
+
+		if (!userAnswer) {
+			return;
+		}
+
+		if (learningMode === 'from') {
+			isCorrect = currentSentence[currentLanguage].some(
+				(translation) => levenshtein(userAnswer.toLowerCase(), translation.toLowerCase()) <= 2
+			);
+		} else {
+			const romajiAnswerMatches = currentSentence.q.match(/\((.*?)\)/);
+			const romajiAnswer = romajiAnswerMatches ? romajiAnswerMatches[1] : '';
+			isCorrect = levenshtein(userAnswer.toLowerCase(), romajiAnswer.toLowerCase()) <= 2;
+		}
+
+		const correctAnswers =
+			learningMode === 'from' ? currentSentence[currentLanguage].join(', ') : currentSentence.q;
+
+		if (isCorrect) {
+			const correctTranslation = getTranslation('correct', 'Correct!');
+			feedbackText = `✅ ${correctTranslation} ${correctAnswers}`;
+			if (mistakes[currentSentence.q]) {
+				mistakes[currentSentence.q]--;
+			}
+			isFeedbackDisplayed = true;
+			submitAnswerButtonText = getTranslation('nextQuestion', 'Next question');
+			correctAudio.play();
+			questionBoxClasses = 'border-green-500';
+			updateStreak(true);
+		} else {
+			const incorrectTranslation = getTranslation('incorrect', 'Incorrect! Correct answers: ');
+			feedbackText = `❌ ${incorrectTranslation} ${correctAnswers}`;
+			if (!mistakes[currentSentence.q]) {
+				mistakes[currentSentence.q] = 1;
+			} else {
+				mistakes[currentSentence.q]++;
+			}
+			isFeedbackDisplayed = true;
+			submitAnswerButtonText = getTranslation('nextQuestion', 'Next question');
+			incorrectAudio.play();
+			questionBoxClasses = 'border-red-500';
+			updateStreak(false);
+		}
+
+		localStorage.setItem('mistakes', JSON.stringify(mistakes));
+
+		currentTimeout ? clearTimeout(currentTimeout) : null;
+
+		currentTimeout = setTimeout(loadNextSentence, 3500);
+	}
+
+	function handleSearchInput() {
+		const searchTerm = searchInputValue;
+
+		filteredSentences = sentences.filter((sentence) => {
+			return (
+				sentence.q.toLowerCase().includes(searchTerm) ||
+				sentence[currentLanguage].join(' / ').toLowerCase().includes(searchTerm)
+			);
+		});
+	}
+
+	function changeLanguage(event: Event) {
+		event.preventDefault();
+		const target = event.currentTarget as HTMLLinkElement;
+		const url = new URL(target.href);
+		goto(url.href, { replaceState: true });
+		const newLanguage = url.searchParams.get('lang') as LanguageCode;
+		currentLanguage = newLanguage;
+		loadNextSentence();
+	}
+
+	function changeMode(event: Event) {
+		event.preventDefault();
+		const target = event.currentTarget as HTMLLinkElement;
+		const url = new URL(target.href);
+		goto(url.href, { replaceState: true });
+		const newMode = url.searchParams.get('mode') as 'from' | 'to';
+		learningMode = newMode;
+		loadNextSentence();
+	}
+
+	onMount(() => {
+		currentLanguage = getCurrentLanguage();
+		learningMode = getMode();
+		sentences = shuffle(data.questions);
+		filteredSentences = [...sentences];
+
+		function getCurrentLanguage() {
+			const lang = new URLSearchParams(window.location.search).get('lang');
+
+			const exists = languages.some((language) => language.code === lang);
+
+			if (exists) {
+				return lang as LanguageCode;
+			}
+
+			if (defaultLanguage) {
+				return defaultLanguage.code;
+			}
+
+			return languages[0].code;
+		}
+
+		function getMode() {
+			return new URLSearchParams(window.location.search).get('mode') === 'to' ? 'to' : 'from';
+		}
+
+		if (localStorage.getItem('mistakes')) {
+			mistakes = JSON.parse(localStorage.getItem('mistakes') || '{}') as Mistakes;
+		}
+
+		loadNextSentence();
+	});
+</script>
+
+<div class="min-h-screen bg-black flex flex-col items-center justify-center p-5">
+	<div class="flex flex-row gap-4">
+		<div class="rounded-xl bg-slate-900 p-4 mb-4 flex flex-row gap-2">
+			{#each languages as language, index}
+				<a
+					href="?lang={language.code}&mode={learningMode}"
+					class="text-white hover:text-pink-400 {language.code === currentLanguage
+						? 'underline'
+						: ''}"
+					on:click={changeLanguage}
+				>
+					{language.label}
+				</a>
+				{#if index < languages.length - 1}
+					<span class="text-white">|</span>
+				{/if}
+			{/each}
+		</div>
+
+		<div class="rounded-xl bg-slate-900 p-4 mb-4">
+			<a
+				id="fromLink"
+				href="?lang={currentLanguage}&mode=from"
+				class="text-white hover:text-pink-400 {learningMode === 'from' ? 'underline' : ''}"
+				on:click={changeMode}
+			>
+				{t('fromLinkText', 'From')}
+			</a>
+			<span class="text-white">|</span>
+			<a
+				id="toLink"
+				href="?lang={currentLanguage}&mode=to"
+				class="text-white hover:text-pink-400 {learningMode === 'to' ? 'underline' : ''}"
+				on:click={changeMode}
+			>
+				{t('toLinkText', 'To')}
+			</a>
+		</div>
+	</div>
+
+	<div
+		class="bg-opacity-60 shadow-lg rounded-xl p-8 w-full max-w-xl border bg-slate-900 transition-colors relative {questionBoxClasses}"
+		id="questionBox"
+	>
+		<!-- display streak of correct answers in top right corner, rounded, with a pink background
+            position it over the corner of the box
+         -->
+		<div
+			class="flex items-center justify-center absolute -top-3 -right-3 md:-top-6 md:-right-6 w-12 h-12 rounded-full text-white px-4 py-2 text-sm font-medium tracking-wider {streakClasses}"
+		>
+			<span id="streak">
+				{streakText || 0}
+			</span>
+		</div>
+		<h1 id="title" class="text-4xl font-bold mb-2 text-pink-400">百 - Hyaku</h1>
+		<h2 id="subTitle" class="text-md font-semibold mb-6 text-purple-500 tracking-wider">
+			{t('mostImportantWords')}
+		</h2>
+
+		<form id="answerForm" class="space-y-4" on:submit={handleAnswerSubmit}>
+			<div id="question" class="text-xl text-white font-medium">
+				{questionText}
+			</div>
+
+			<div>
+				<input
+					type="text"
+					id="answer"
+					placeholder={t('answer')}
+					required
+					class="w-full p-3 border border-purple-400 bg-opacity-50 bg-slate-800 text-white rounded-md focus:outline-none focus:border-pink-400 focus:ring focus:ring-purple-300 transition duration-150 ease-in-out"
+					bind:value={answerInputValue}
+				/>
+			</div>
+
+			<div>
+				<button
+					type="submit"
+					id="submitAnswerButton"
+					class="bg-purple-600 text-white px-6 py-3 rounded-md hover:bg-pink-600 focus:outline-none focus:border-pink-700 focus:ring focus:ring-purple-400 active:bg-pink-700 transition duration-150 ease-in-out"
+				>
+					{t('submitAnswerButton')}
+				</button>
+			</div>
+
+			<div id="feedback" class="text-lg text-pink-400 font-medium">
+				{feedbackText}
+			</div>
+		</form>
+	</div>
+
+	<div class="mt-8 bg-slate-900">
+		<form class="flex items-center justify-center space-x-4 p-4">
+			<input
+				type="text"
+				id="searchCheatsheet"
+				placeholder={t('search')}
+				class="w-full p-3 border border-purple-400 bg-opacity-50 bg-slate-800 text-white rounded-md focus:outline-none focus:border-pink-400 focus:ring focus:ring-purple-300 transition duration-150 ease-in-out"
+				bind:value={searchInputValue}
+				on:input={handleSearchInput}
+			/>
+		</form>
+
+		<ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4" id="cheatsheet">
+			{#each filteredSentences as sentence}
+				<li
+					class="bg-slate-800 p-4 rounded-md shadow-md text-white text-lg font-medium flex items-center justify-between"
+				>
+					<div class="text-xl">{sentence.q}</div>
+					<div class="text-pink-400">{sentence[currentLanguage].join(' / ')}</div>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</div>
+
+<audio id="correctAudio" src="correct.m4a" />
+<audio id="incorrectAudio" src="incorrect.m4a" />
