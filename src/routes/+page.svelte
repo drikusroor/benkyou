c03e8624d548d90b28d0a41a9d633f8622a15158
Drikus Roor <drikusroor@gmail.com>
2023-10-31 16:00:20 +0100

    fix: Fix main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-4xl font-bold text-center">/h1> Welcome to SvelteKit</h1>
+<h1 class="text-4xl font-bold text-center">Welcome to SvelteKit</h1>
 <p class="text-center">
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
