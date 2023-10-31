3e49b60cf68f2936a1ba3629ba8b226bf0dc724c
Drikus Roor <drikusroor@gmail.com>
2023-10-31 15:43:25 +0100

    config: Set fallback to index.html

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			precompress: true,
-			fallback: '/japanese',
+			fallback: 'index.html'
 		})
 	}
 };
