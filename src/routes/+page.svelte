96654a8eacbd5ee58d23cc008e3ff4cbf80a2f70
Drikus Roor <drikusroor@gmail.com>
2023-11-02 14:21:27 +0100

    style: Style homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
-	import { base } from "$app/paths";
+	import { base } from '$app/paths';
 </script>
 
-<h1 class="text-4xl font-bold text-center">Welcome to Hyaku</h1>
-<p class="text-center">
-	This site allows you to learn Japanese and Esperanto vocabulary in a fun and easy way.
-</p>
+<div class="min-h-screen bg-black flex flex-col items-center p-5">
+	<div
+		class="bg-opacity-60 shadow-lg rounded-xl p-8 w-full max-w-xl border bg-slate-900 transition-colors relative"
+		id="questionBox"
+	>
+		<h1 id="title" class="text-4xl font-bold mb-2 text-pink-400">百 - Hyaku</h1>
+		<h2 id="subTitle" class="text-md font-semibold text-purple-500 tracking-wider">
+			This site allows you to learn Japanese and Esperanto vocabulary in a fun and easy way.
+		</h2>
+	</div>
 
-<div class="flex justify-center mt-4 gap-4">
-	<a href="{base}/japanese" class="text-2xl font-bold text-center hover:text-blue-500 transition-colors duration-200 ease-in-out border-b-2 border-blue-500">Japanese</a>
-		
-	<a href="{base}/esperanto" class="text-2xl font-bold text-center hover:text-blue-500 transition-colors duration-200 ease-in-out border-b-2 border-blue-500">Esperanto</a>
+	<div class="flex justify-center mt-8 gap-8 bg-slate-900 p-8 rounded-lg">
+		<a
+			href="{base}/japanese"
+			class="bg-purple-600 text-white px-6 py-3 rounded-md hover:bg-pink-600 focus:outline-none focus:border-pink-700 focus:ring focus:ring-purple-400 active:bg-pink-700 transition duration-150 ease-in-out"
+			>Japanese</a
+		>
+
+		<a
+			href="{base}/esperanto"
+			class="bg-purple-600 text-white px-6 py-3 rounded-md hover:bg-pink-600 focus:outline-none focus:border-pink-700 focus:ring focus:ring-purple-400 active:bg-pink-700 transition duration-150 ease-in-out"
+			>Esperanto</a
+		>
+	</div>
 </div>
