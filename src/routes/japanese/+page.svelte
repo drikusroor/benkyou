1638a249c36a947576bfd6b122aee6e3b9a213ad
Drikus Roor <drikusroor@gmail.com>
2023-11-02 10:46:21 +0100

    fix: Fix CSS import

--- src/routes/japanese/+page.svelte
+++ src/routes/japanese/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import FlashCards from "../../components/FlashCards.svelte";
+	import FlashCards from '../../components/FlashCards.svelte';
 
-	import data from "./data.json";
+	import data from './data.json';
 </script>
 
 <FlashCards flashCardsConfigurationData={data} />
