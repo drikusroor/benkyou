dc1f67e4bb70e0b9b34301a70086b50e41c2e716
Drikus Roor <drikusroor@gmail.com>
2023-11-02 14:25:19 +0100

    ci: Make sure BASE_PATH env variable gets taken

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -21,7 +19,7 @@ const config = {
             strict: true
         }),
         paths: {
-            base: dev ? '' : process.env.BASE_PATH,
+            base: process.env.BASE_PATH || '',
         }
 	}
 };
